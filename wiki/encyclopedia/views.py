7f0c62a7d7ede2518ec6b8a7f704ab3923713a62
Gaurav J <gauravjayaraj@gmail.com>
2021-07-01 01:31:01 +0530

    Index page , Entry page(s), search functionality CONFIGURED

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+# for forms
+from django import forms
 
 from . import util
 import encyclopedia
@@ -10,10 +14,50 @@ import markdown
 
 md = markdown.Markdown()
 
+class Search(forms.Form):
+    item = forms.CharField(widget=forms.TextInput(attrs={'class' : 'search', 'placeholder': 'Search'}))
+
+
 def index(request):
-    return render(request, "encyclopedia\index.html", {
-        "entries": util.list_entries()
-    })
+    entries = util.list_entries()       # list of all entries
+
+    lookingFor = []             # list of hints/possible/related entries for search result page
+
+    if request.method == 'POST':
+        form = Search(request.POST)        # get the posted object and it's data
+        
+        if form.is_valid():                 # check the CSRF validation (cros site request forgery)
+            item = form.cleaned_data['item']
+
+            
+            if item in entries:
+                page = md.convert(util.get_entry(item))
+                return render(request,"encyclopedia/entry.html",{
+                    "page" : page,
+                    "title": item
+                })
+
+            else:                       # if searched entry not found either return the search page or redired to error page
+                for i in entries:
+                    if item.lower() in i.lower():
+                        lookingFor.append(i)
+                    
+                return render(request, "encyclopedia/search.html",{
+                    "searEntries":lookingFor,
+                    "form": Search()
+                })
+        
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": entries, 
+                "form":form
+            })
+
+    else:           # GET request
+        return render(request, "encyclopedia/index.html", {         
+            "entries": entries, 
+            "form":Search()  
+        })
 
 
 def entry(request,title):
